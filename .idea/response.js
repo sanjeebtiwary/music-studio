5fe9a62f666bbe5b03294008f04bf277ece0bd95
ARYAN KUMAR SINGH <98681477+aryansingh-123@users.noreply.github.com>
2022-10-17 09:33:18 +0530

    js part

---
+++ .idea/response.js
@@ -0,0 +1,6 @@
+let now_playing = document.querySelector(".now-playing");
+let track_name = document.querySelector(".track-name");
+let track = document.querySelector(".track");
+let track_artist = document.querySelector(".artist");
+
+let
